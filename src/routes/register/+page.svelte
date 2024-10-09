942e2ed860051f176fe7cb0b95853d3aae285bba
kyleg <kylegianlatina@gmail.com>
2024-10-09 20:21:50 +0800

    fixed register bug

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -3,23 +3,6 @@
     import SectionWrapper from "../../components/SectionWrapper.svelte";
     import { onMount } from "svelte";
     import { goto } from "$app/navigation";
-    onMount(async() => {
-            const url = "https://studyplan-api.onrender.com/api/v1/access"
-            const data = await fetch(url, {
-                method: "GET",
-                credentials: 'include',
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded"
-                },
-            })
-            const response = await data.json()
-            if (data.ok) {
-                sessionStorage.setItem("access_token", response.access_token)
-            } else {
-                goto('./')
-            }
-        }
-    )
     const handleRegister = async(email, fname, lname, password) => {
         const url = "https://studyplan-api.onrender.com/api/v1/register"
         const payload = {
